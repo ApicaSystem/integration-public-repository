1d0e359c66346f2587341494a51a31ff9655e757
Pat Wakeem <patwakeem@gmail.com>
2022-01-03 12:05:30 +0100

    Update test-js.js

--- test-js.js
+++ test-js.js
@@ -2,8 +2,6 @@ var start = Date.now() / 1000;
 var end = start + 1;
 var test = {
  "returncode": 0,
- "stdout": "Javascript stdout",
- "stderr": "Javascript stderr",
  "start_time": start,
  "end_time": end,
  "message": "Command message from javascript",
